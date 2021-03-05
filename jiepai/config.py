7a98e611f9163803848ab70f731e5415134a4d12
lavwaqq939 <zhuaicangku17@163.com>
2021-03-05 23:00:36 +0800

    添加配置文件config.py

---
+++ jiepai/config.py
@@ -0,0 +1,15 @@
+SSH_IP = '47.106.9.8'
+SSH_PORT = 22
+SSH_USER = "root"
+SSH_PASSWORD = "jIN13462967465"
+MONGO_IP = 'localhost'
+MONGO_PORT = 27017
+MONGO_DB = 'toutiao'
+MONGO_TABLE = 'toutiao'
+
+SAVE_FILE_PATH = 'F:/spider/jiepai'
+
+GROUP_START = 1
+GROUP_END = 20
+
+KEYWORD = '街拍'
